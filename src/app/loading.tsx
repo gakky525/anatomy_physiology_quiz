6245c2b145e9e9746f863f2d4dd5fa2758d3b038
gakky525 <yuudai.t0525@icloud.com>
2025-04-18 17:14:35 +0900

    ローディングUIの修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,7 @@
-import { ClipLoader } from 'react-spinners';
-
 export default function Loading() {
   return (
     <div className='flex flex-col items-center justify-center h-96 text-xl text-gray-600'>
-      <ClipLoader color='#36d7b7' size={40} className='mb-4' />
+      <div className='animate-spin rounded-full h-10 w-10 border-4 border-blue-400 border-t-transparent mb-4' />
       <span>読み込み中...</span>
     </div>
   );
